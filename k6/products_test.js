814baec7503729f25e7b1371b26781cc8964baac
Sara Li <ruimin.li77@gmail.com>
2023-05-30 17:14:22 -0700

    finish K6 stress tests

--- k6/products_test.js
+++ k6/products_test.js
@@ -3,15 +3,12 @@ import { check, sleep } from 'k6'
 
 export const options = {
   stages: [
-    { duration: '30s', target: 1 },
-    { duration: '30s', target: 10 },
-    { duration: '30s', target: 100 },
     { duration: '30s', target: 1000 },
   ],
 };
 
 export default function () {
-  const p = Math.floor(Math.random() * 1000) + 1;
+  const p = Math.floor(Math.random() * 10000) + 1;
   const res = http.get(`http://localhost:3000/products?page=${p}&count=100`);
 
   check(res, { 'success login': (r) => r.status === 200 });
